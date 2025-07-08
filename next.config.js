4732620cb147cf98453399688e3e6974417bda1f
steve jaju <stevejacob98@gmail.com>
2025-07-08 23:48:43 +0000

    another issue that I am seeing in the website after deployment is Map Er

--- next.config.js
+++ next.config.js
@@ -1,7 +1,11 @@
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
+  env: {
+    NEXT_PUBLIC_GOOGLE_CLIENT_API_KEY: process.env.NEXT_PUBLIC_GOOGLE_CLIENT_API_KEY,
+    NEXT_PUBLIC_SUPABASE_URL: process.env.NEXT_PUBLIC_SUPABASE_URL,
+    NEXT_PUBLIC_SUPABASE_ANON_KEY: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
+  },
   typescript: {
     ignoreBuildErrors: true,
   },
