13f77168275520e7e1027f77fc2a8ba5e9ee6240
steve jaju <stevejacob98@gmail.com>
2025-07-08 23:33:30 +0000

    I am trying to deploy the app in firebase console. But the build fails,

--- src/lib/supabase/client.ts
+++ src/lib/supabase/client.ts
@@ -4,7 +4,7 @@ import type { Database } from '../database.types'
 
 export function createSupabaseBrowserClient() {
   return createBrowserClient<Database>(
-    process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+    process.env.NEXT_PUBLIC_SUPABASE_URL || "http://localhost:54321",
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || "dummykey"
   )
 }
