957d5daca8ea76c98a3ff90680c2ac0a7053e0f6
steve jaju <stevejacob98@gmail.com>
2025-07-08 21:37:18 +0000

    npm run dev
    > nextn@0.1.0 dev
    > next dev
    
    >>> [next.config.js] Starting

---
+++ src/app/test/page.tsx
@@ -0,0 +1,8 @@
+
+export default function TestPage() {
+  return (
+    <main className="flex items-center justify-center min-h-screen">
+      <h1 className="text-4xl font-bold font-headline text-white">Test Page Works!</h1>
+    </main>
+  );
+}
