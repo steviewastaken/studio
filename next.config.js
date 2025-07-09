9337caa31d2ede373e08a4995ba6ba3bf3cea586
steve jaju <stevejacob98@gmail.com>
2025-07-09 15:08:03 +0000

    i need AI Fraud & Abuse Detection mention mentoned on the homepage like

--- next.config.js
+++ next.config.js
@@ -2,9 +2,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   env: {
-    NEXT_PUBLIC_GOOGLE_CLIENT_API_KEY: process.env.NEXT_PUBLIC_GOOGLE_CLIENT_API_KEY,
+    NEXT_PUBLIC_GOOGLE_CLIENT_API_KEY: 'AIzaSyBjbd25PsMgE3airzSJRWhe9I9G1_IeOP0',
     NEXT_PUBLIC_SUPABASE_URL: process.env.NEXT_PUBLIC_SUPABASE_URL,
     NEXT_PUBLIC_SUPABASE_ANON_KEY: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
+    GOOGLE_SERVER_API_KEY: 'AIzaSyDirKm7OeYmSxcmcHELb-z4U1SjJm8S4SI',
   },
   typescript: {
     ignoreBuildErrors: true,
